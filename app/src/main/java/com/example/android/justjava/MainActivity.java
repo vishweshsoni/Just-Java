824a9f2928f1baccd28cf3bf56e1992b12ebd7c0
vishwesh <vishweshsoni@gmail.com>
2018-03-13 02:47:49 +0530

    coffee order

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        displayPrice(quantity*5);
+
+    }
+
+    private void display(int i) {
+        TextView textView=(TextView)findViewById(R.id.quantity_text_view);
+        textView.setText(""+i);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void decrement(View view) {
+
+        quantity=quantity-1;
+        display(quantity);
+    }
+
+    public void increment(View view) {
+
+        quantity=quantity+1;
+        display(quantity);
+    }
+}
